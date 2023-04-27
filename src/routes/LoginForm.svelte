c2e9c3d1e6d7a17f533a32a39c9c8c8e7b215a1e
ej-agas <agas.ej09@gmail.com>
2023-04-27 23:20:24 +0800

    make use of svelte superforms
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/routes/LoginForm.svelte
+++ src/routes/LoginForm.svelte
@@ -3,48 +3,55 @@
 	import SignupForm from './SignupForm.svelte';
 	import { goto } from '$app/navigation';
 	import Login from '../api/Login';
-	import globalStore from '$stores/Global';
+	import { superForm } from 'sveltekit-superforms/client';
+	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
 
 	let loginForm: App.LoginForm = {
 		email: '',
 		password: ''
 	};
 
-	let errors: App.ValidationErrors = {
-		email: '',
-		password: '',
-		message: ''
-	};
-
-	function clearErrors() {
-		errors = {
-			email: '',
-			password: '',
-			message: ''
-		};
-	}
+	const { form, errors, enhance, constraints } = superForm(loginForm, {
+		SPA: true,
+		taintedMessage: 'Are you sure you want to leave this page? You have unsaved changes.',
+		validators: {
+			email: (value) =>
+				/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+					? null
+					: 'Invalid email address.',
+			password: (value) => (value.length > 5 ? null : 'Password must be at least 6 characters.')
+		},
+		onUpdate({ form }) {
+			if (form.valid === true) {
+				handleLogin();
+			}
+		}
+	});
 
 	async function handleLogin() {
-		clearErrors();
-
-		const response = await Login(loginForm);
+		const response = await Login({
+			email: $form.email,
+			password: $form.password
+		});
 
 		if (!response.ok) {
-			const error = await response.json();
+			const errJSON = await response.json();
 
-			if (error.errors) {
-				const fields = Object.keys(error.errors);
+			if (errJSON.message) {
+				$errors.message = errJSON.message;
+				return;
+			}
+
+			if (errJSON.errors) {
+				const fields = Object.keys(errJSON.errors);
 				fields.forEach((field) => {
-					errors[field] = error.errors[field][0];
+					$errors[field] = errJSON.errors[field][0];
 				});
-
 				return;
 			}
 
-			if (error.message) {
-				errors.message = error.message;
-				return;
-			}
+			$errors.message = ['An unknown error occurred. check the logs for details.'];
+			console.error(errJSON);
 		}
 
 		const data = await response.json();
@@ -63,38 +70,41 @@
 	}
 </script>
 
+<SuperDebug data={$form} />
+
 <div class="modal-example-form card p-8 w-modal shadow-xl space-y-4">
 	<form
 		class="modal-form border border-surface-500 p-8 space-y-4 rounded-container-token flex flex-col"
-		on:submit|preventDefault={handleLogin}
+		method="POST"
+		use:enhance
 	>
 		<label class="label">
 			<span class="">Email</span>
 			<input
 				class="input p-4"
-				class:input-error={errors.email}
+				class:input-error={$errors.email}
 				type="text"
-				bind:value={loginForm.email}
+				bind:value={$form.email}
 			/>
-			{#if errors.email}
-				<p class="err-text">{errors.email}</p>
+			{#if $errors.email}
+				<p class="err-text">{$errors.email}</p>
 			{/if}
 		</label>
 		<label class="label">
 			<span class="">Password</span>
 			<input
 				class="input p-4"
-				class:input-error={errors.password}
+				class:input-error={$errors.password}
 				type="password"
-				bind:value={loginForm.password}
+				bind:value={$form.password}
 			/>
-			{#if errors.password}
-				<p class="err-text">{errors.password}</p>
+			{#if $errors.password}
+				<p class="err-text">{$errors.password}</p>
 			{/if}
 		</label>
 		<button class="btn variant-filled p-4">Log in</button>
-		{#if errors.message}
-			<p class="err-text bg-error-50 p-4 rounded-md">{errors.message}</p>
+		{#if $errors.message}
+			<p class="err-text bg-error-50 p-4 rounded-md">{$errors.message}</p>
 		{/if}
 	</form>
 	<footer class="modal-footer">
