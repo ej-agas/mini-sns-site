9095be44c54e6535cb1e8da9e2a071f9594b53b3
ej-agas <agas.ej09@gmail.com>
2023-05-25 23:55:57 +0800

    WIP user profile page
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

---
+++ src/api/GetUserProfile.ts
@@ -0,0 +1,30 @@
+import APIBaseURL from './APIBaseURL';
+
+async function GetUserProfile(userId: string): Promise<App.UserProfile> {
+	const response = await fetch(APIBaseURL() + `/api/v1/users/${userId}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + localStorage.getItem('token')
+		}
+	});
+
+	const data = await response.json();
+
+	return {
+		id: data.id,
+		fullName: data.full_name,
+		firstName: data.first_name,
+		middleName: data.middle_name,
+		lastName: data.last_name,
+		bio: data.bio,
+		email: data.email,
+		isVerified: data.is_verified,
+		verifiedDate: data.verified_date,
+		joinDate: data.join_date,
+		userName: data.username,
+		picture: data.picture
+	};
+}
+
+export default GetUserProfile;
---
+++ src/routes/(authenticated)/users/[userId]/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import userProfile from '$stores/UserProfile';
+	import { Avatar, modalStore } from '@skeletonlabs/skeleton';
+	import dayjs from 'dayjs';
+	import advancedFormat from 'dayjs/plugin/advancedFormat';
+	import timezone from 'dayjs/plugin/timezone';
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	import Spinner from '$lib/Spinner.svelte';
+	import { page } from '$app/stores';
+	import GetUserProfile from '$api/GetUserProfile';
+
+	dayjs.extend(timezone);
+	dayjs.extend(advancedFormat);
+
+	let isLoading: boolean = true;
+	let posts: App.Post[] = [];
+
+	onMount(async () => {
+		const userProfile = await GetUserProfile($page.params.userId);
+		console.log(userProfile);
+		isLoading = false;
+	});
+</script>
+
+<div class="p-4 m-4 bg-surface-600 rounded-xl grid grid-cols-1 sm:grid-cols-2 gap-4">
+	<div class="rounded-xl card p-4 grid justify-items-center">
+		<Avatar src="https://i.pravatar.cc/?img=48" width="w-32" rounded="rounded" />
+		<h1>{$userProfile.fullName}</h1>
+		<h3>{$userProfile.bio}</h3>
+	</div>
+	<div class="rounded-xl card p-4">
+		<div class="grid grid-cols-1">
+			<h4>First Name: {$userProfile.firstName}</h4>
+			<h4>Middle Name: {$userProfile.middleName}</h4>
+			<h4>Last Name: {$userProfile.lastName}</h4>
+			<h4>Email: {$userProfile.email}</h4>
+			<h4>Join Date: {dayjs($userProfile.joinDate).format('MMMM DD, YYYY h:mm A z')}</h4>
+			<h4>
+				Is Verified: <span
+					class={'chip ' +
+						($userProfile.isVerified ? 'variant-filled-success' : 'variant-chip-warning')}
+					>{$userProfile.isVerified ? 'Verified' : 'Unverified'}</span
+				>
+			</h4>
+			{#if $userProfile.isVerified}
+				<h4>
+					Verified Date: {dayjs($userProfile.verifiedDate).format('MMMM DD, YYYY h:mm A z')}
+				</h4>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<div class="feed">
+	{#if isLoading}
+		<div class="p-4 m-4 bg-surface-600 rounded-xl grid">
+			<div class="flex flex-col items-center">
+				<Spinner />
+			</div>
+		</div>
+	{:else}
+		{#each posts as post}
+			<div class="p-4 m-4 bg-surface-600 rounded-xl grid" transition:fade={{ duration: 200 }}>
+				<div class="flex flex-col">
+					<Avatar src="https://i.pravatar.cc/?img=48" width="w-10" rounded="rounded-full" />
+					<div class="text-2xl text-white">Title: {post.title}</div>
+					<div class="text-xl text-white">Body: {post.body}</div>
+					<div class="text-xl">User ID: {post.userId}</div>
+					<div class="text-md text-white justify-items-end">
+						Created At: {dayjs(post.createdAt).format('MMM D, YYYY h:mm:ss a')}
+					</div>
+				</div>
+			</div>
+		{/each}
+	{/if}
+</div>
