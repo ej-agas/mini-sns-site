b32c60a161f8a3f2aebc992a67cd02a94fda3c5e
ej-agas <agas.ej09@gmail.com>
2023-05-29 23:34:36 +0800

    Add profile page of users
    Signed-off-by: ej-agas <agas.ej09@gmail.com>

--- src/routes/(authenticated)/users/[userId]/+page.svelte
+++ src/routes/(authenticated)/users/[userId]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import userProfile from '$stores/UserProfile';
-	import { Avatar, modalStore } from '@skeletonlabs/skeleton';
+	import { AppBar, Avatar, modalStore } from '@skeletonlabs/skeleton';
 	import dayjs from 'dayjs';
 	import advancedFormat from 'dayjs/plugin/advancedFormat';
 	import timezone from 'dayjs/plugin/timezone';
@@ -9,57 +8,73 @@
 	import Spinner from '$lib/Spinner.svelte';
 	import { page } from '$app/stores';
 	import GetUserProfile from '$api/GetUserProfile';
+	import { error } from '@sveltejs/kit';
 
 	dayjs.extend(timezone);
 	dayjs.extend(advancedFormat);
 
 	let isLoading: boolean = true;
 	let posts: App.Post[] = [];
+	let userProfile: App.UserProfile;
+
+	let isError: boolean = false;
 
 	onMount(async () => {
-		const userProfile = await GetUserProfile($page.params.userId);
-		console.log(userProfile);
+		try {
+			userProfile = await GetUserProfile($page.params.userId);
+		} catch (error) {
+			isLoading = false;
+			isError = true;
+			return;
+		}
+
 		isLoading = false;
 	});
 </script>
 
-<div class="p-4 m-4 bg-surface-600 rounded-xl grid grid-cols-1 sm:grid-cols-2 gap-4">
-	<div class="rounded-xl card p-4 grid justify-items-center">
-		<Avatar src="https://i.pravatar.cc/?img=48" width="w-32" rounded="rounded" />
-		<h1>{$userProfile.fullName}</h1>
-		<h3>{$userProfile.bio}</h3>
-	</div>
-	<div class="rounded-xl card p-4">
-		<div class="grid grid-cols-1">
-			<h4>First Name: {$userProfile.firstName}</h4>
-			<h4>Middle Name: {$userProfile.middleName}</h4>
-			<h4>Last Name: {$userProfile.lastName}</h4>
-			<h4>Email: {$userProfile.email}</h4>
-			<h4>Join Date: {dayjs($userProfile.joinDate).format('MMMM DD, YYYY h:mm A z')}</h4>
-			<h4>
-				Is Verified: <span
-					class={'chip ' +
-						($userProfile.isVerified ? 'variant-filled-success' : 'variant-chip-warning')}
-					>{$userProfile.isVerified ? 'Verified' : 'Unverified'}</span
-				>
-			</h4>
-			{#if $userProfile.isVerified}
-				<h4>
-					Verified Date: {dayjs($userProfile.verifiedDate).format('MMMM DD, YYYY h:mm A z')}
-				</h4>
-			{/if}
+{#if isLoading}
+	<div class="mt-8 p-4 m-4 bg-surface-600 rounded-xl grid">
+		<div class="flex flex-col items-center">
+			<Spinner />
 		</div>
 	</div>
-</div>
-
-<div class="feed">
-	{#if isLoading}
-		<div class="p-4 m-4 bg-surface-600 rounded-xl grid">
-			<div class="flex flex-col items-center">
-				<Spinner />
+{:else if isError}
+	<div class="mt-8 p-4 m-4 bg-surface-600 rounded-xl grid">
+		<div class="flex flex-col items-center">
+			<h2>Profile Not Found</h2>
+		</div>
+	</div>
+{:else}
+	<div class="p-4 m-4 bg-surface-600 rounded-xl grid grid-cols-1 sm:grid-cols-2 gap-4">
+		<div class="rounded-xl card p-4 grid justify-items-center">
+			<Avatar src="https://i.pravatar.cc/?img=48" width="w-32" rounded="rounded" />
+			<h1>{userProfile.fullName}</h1>
+			<h3>{userProfile.bio}</h3>
+		</div>
+		<div class="rounded-xl card p-4">
+			<div class="grid grid-cols-1">
+				<h4>First Name: {userProfile.firstName}</h4>
+				<h4>Middle Name: {userProfile.middleName}</h4>
+				<h4>Last Name: {userProfile.lastName}</h4>
+				<h4>Email: {userProfile.email}</h4>
+				<h4>Join Date: {dayjs(userProfile.joinDate).format('MMMM DD, YYYY h:mm A z')}</h4>
+				<h4>
+					Is Verified: <span
+						class={'chip ' +
+							(userProfile.isVerified ? 'variant-filled-success' : 'variant-chip-warning')}
+						>{userProfile.isVerified ? 'Verified' : 'Unverified'}</span
+					>
+				</h4>
+				{#if userProfile.isVerified}
+					<h4>
+						Verified Date: {dayjs(userProfile.verifiedDate).format('MMMM DD, YYYY h:mm A z')}
+					</h4>
+				{/if}
 			</div>
 		</div>
-	{:else}
+	</div>
+
+	<div class="feed">
 		{#each posts as post}
 			<div class="p-4 m-4 bg-surface-600 rounded-xl grid" transition:fade={{ duration: 200 }}>
 				<div class="flex flex-col">
@@ -73,5 +88,5 @@
 				</div>
 			</div>
 		{/each}
-	{/if}
-</div>
+	</div>
+{/if}
